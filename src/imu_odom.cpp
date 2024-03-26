3bcd79bcc1adcd77e8c375e897031305196e7c7a
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2024-03-25 20:23:59 -0500

    IMU republisher now runs.

--- src/imu_odom.cpp
+++ src/imu_odom.cpp
@@ -35,30 +35,34 @@ public:
     }
 };
 
-std::shared_ptr<Custom> custom_ptr;
-
 class Go1ImuNode : public rclcpp::Node
 {
 public:
     Go1ImuNode() : Node("go1_imu")
     {
+		RCLCPP_INFO(this->get_logger(), "Initializing Go1ImuNode.");
+
+		// Start the Go1 SDK.
+		this->go1_sdk = std::make_shared<Custom>();
+		RCLCPP_INFO(this->get_logger(), "Unitree Go1 SDK initialized.");
+
+		// Create publishers and subscribers.
         pub_imu = this->create_publisher<sensor_msgs::msg::Imu>("imu", 1);
         pub_odom = this->create_publisher<nav_msgs::msg::Odometry>("odom", 1);
         tf_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
-        loop_udpSend = this->create_wall_timer(std::chrono::milliseconds(2), std::bind(&Custom::highUdpSend, custom_ptr));
-        loop_udpRecv = this->create_wall_timer(std::chrono::milliseconds(2), std::bind(&Custom::highUdpRecv, custom_ptr));
-
-        loop_rate = std::make_shared<rclcpp::Rate>(100);
+		// Create timers.
+        loop_udpSend = this->create_wall_timer(std::chrono::milliseconds(2), std::bind(&Custom::highUdpSend, this->go1_sdk));
+        loop_udpRecv = this->create_wall_timer(std::chrono::milliseconds(2), std::bind(&Custom::highUdpRecv, this->go1_sdk));
     }
 
 private:
+	std::shared_ptr<Custom> go1_sdk;
     rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr pub_imu;
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_odom;
     std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
     rclcpp::TimerBase::SharedPtr loop_udpSend;
     rclcpp::TimerBase::SharedPtr loop_udpRecv;
-    std::shared_ptr<rclcpp::Rate> loop_rate;
 
     long count = 0;
 
@@ -71,18 +75,18 @@ private:
         msg_imu.header.stamp = current_time;
         msg_imu.header.frame_id = "imu_link";
 
-        msg_imu.orientation.w = custom_ptr->high_state.imu.quaternion[0];
-        msg_imu.orientation.x = custom_ptr->high_state.imu.quaternion[1];
-        msg_imu.orientation.y = custom_ptr->high_state.imu.quaternion[2];
-        msg_imu.orientation.z = custom_ptr->high_state.imu.quaternion[3];
+        msg_imu.orientation.w = this->go1_sdk->high_state.imu.quaternion[0];
+        msg_imu.orientation.x = this->go1_sdk->high_state.imu.quaternion[1];
+        msg_imu.orientation.y = this->go1_sdk->high_state.imu.quaternion[2];
+        msg_imu.orientation.z = this->go1_sdk->high_state.imu.quaternion[3];
 
-        msg_imu.angular_velocity.x = custom_ptr->high_state.imu.gyroscope[0];
-        msg_imu.angular_velocity.y = custom_ptr->high_state.imu.gyroscope[1];
-        msg_imu.angular_velocity.z = custom_ptr->high_state.imu.gyroscope[2];
+        msg_imu.angular_velocity.x = this->go1_sdk->high_state.imu.gyroscope[0];
+        msg_imu.angular_velocity.y = this->go1_sdk->high_state.imu.gyroscope[1];
+        msg_imu.angular_velocity.z = this->go1_sdk->high_state.imu.gyroscope[2];
 
-        msg_imu.linear_acceleration.x = custom_ptr->high_state.imu.accelerometer[0];
-        msg_imu.linear_acceleration.y = custom_ptr->high_state.imu.accelerometer[1];
-        msg_imu.linear_acceleration.z = custom_ptr->high_state.imu.accelerometer[2];
+        msg_imu.linear_acceleration.x = this->go1_sdk->high_state.imu.accelerometer[0];
+        msg_imu.linear_acceleration.y = this->go1_sdk->high_state.imu.accelerometer[1];
+        msg_imu.linear_acceleration.z = this->go1_sdk->high_state.imu.accelerometer[2];
 
         pub_imu->publish(msg_imu);
 
@@ -92,18 +96,18 @@ private:
         msg_odom.header.frame_id = "odom";
         msg_odom.child_frame_id = "base_link";
 
-        msg_odom.pose.pose.position.x = custom_ptr->high_state.position[0];
-        msg_odom.pose.pose.position.y = custom_ptr->high_state.position[1];
-        msg_odom.pose.pose.position.z = custom_ptr->high_state.position[2];
+        msg_odom.pose.pose.position.x = this->go1_sdk->high_state.position[0];
+        msg_odom.pose.pose.position.y = this->go1_sdk->high_state.position[1];
+        msg_odom.pose.pose.position.z = this->go1_sdk->high_state.position[2];
 
-        msg_odom.pose.pose.orientation.w = custom_ptr->high_state.imu.quaternion[0];
-        msg_odom.pose.pose.orientation.x = custom_ptr->high_state.imu.quaternion[1];
-        msg_odom.pose.pose.orientation.y = custom_ptr->high_state.imu.quaternion[2];
-        msg_odom.pose.pose.orientation.z = custom_ptr->high_state.imu.quaternion[3];
+        msg_odom.pose.pose.orientation.w = this->go1_sdk->high_state.imu.quaternion[0];
+        msg_odom.pose.pose.orientation.x = this->go1_sdk->high_state.imu.quaternion[1];
+        msg_odom.pose.pose.orientation.y = this->go1_sdk->high_state.imu.quaternion[2];
+        msg_odom.pose.pose.orientation.z = this->go1_sdk->high_state.imu.quaternion[3];
 
-        msg_odom.twist.twist.linear.x = custom_ptr->high_state.velocity[0];
-        msg_odom.twist.twist.linear.y = custom_ptr->high_state.velocity[1];
-        msg_odom.twist.twist.angular.z = custom_ptr->high_state.velocity[2];
+        msg_odom.twist.twist.linear.x = this->go1_sdk->high_state.velocity[0];
+        msg_odom.twist.twist.linear.y = this->go1_sdk->high_state.velocity[1];
+        msg_odom.twist.twist.angular.z = this->go1_sdk->high_state.velocity[2];
 
         pub_odom->publish(msg_odom);
 
@@ -113,13 +117,13 @@ private:
         odom_trans.header.frame_id = "odom";
         odom_trans.child_frame_id = "base_link";
 
-        odom_trans.transform.translation.x = custom_ptr->high_state.position[0];
-        odom_trans.transform.translation.y = custom_ptr->high_state.position[1];
-        odom_trans.transform.translation.z = custom_ptr->high_state.position[2];
-        odom_trans.transform.rotation.w = custom_ptr->high_state.imu.quaternion[0];
-        odom_trans.transform.rotation.x = custom_ptr->high_state.imu.quaternion[1];
-        odom_trans.transform.rotation.y = custom_ptr->high_state.imu.quaternion[2];
-        odom_trans.transform.rotation.z = custom_ptr->high_state.imu.quaternion[3];
+        odom_trans.transform.translation.x = this->go1_sdk->high_state.position[0];
+        odom_trans.transform.translation.y = this->go1_sdk->high_state.position[1];
+        odom_trans.transform.translation.z = this->go1_sdk->high_state.position[2];
+        odom_trans.transform.rotation.w = this->go1_sdk->high_state.imu.quaternion[0];
+        odom_trans.transform.rotation.x = this->go1_sdk->high_state.imu.quaternion[1];
+        odom_trans.transform.rotation.y = this->go1_sdk->high_state.imu.quaternion[2];
+        odom_trans.transform.rotation.z = this->go1_sdk->high_state.imu.quaternion[3];
 
         tf_broadcaster->sendTransform(odom_trans);
 
@@ -130,7 +134,6 @@ private:
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    custom_ptr = std::make_shared<Custom>();
     auto node = std::make_shared<Go1ImuNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
